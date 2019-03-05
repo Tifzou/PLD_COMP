5d06b25ca8b70a5b09772a5bd53801907a0e3725
Tifenn Floch <floch.tifenn@yahoo.fr>
2019-03-05 09:53:30 +0100

    visitProg

--- Visiteur.h
+++ Visiteur.h
@@ -7,7 +7,13 @@ using namespace std;
 
 class Visiteur: public ExprBaseVisitor{
 public:
-	virtual antlrcpp::Any visitConst(ExprParser::ConstContext *ctx) override{
+	antlrcpp::Any visitProg(ExprParser::ProgContext *ctx) override 
+	{
+		return (int) visit(ctx->expr());
+	}
+
+	virtual antlrcpp::Any visitConst(ExprParser::ConstContext *ctx) override
+	{
 	     return (int) stoi(ctx->INT()getText());
 	}
 };
