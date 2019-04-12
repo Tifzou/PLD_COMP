c15f548ee56e153da6d75bde771e9b380016b3f0
AnatolyG6 <anatolygasiuk@gmail.com>
2019-04-12 07:40:09 +0200

    if else simple (imbrication dans if ) - marche

--- index.c
+++ index.c
@@ -1,14 +1,27 @@
 int main()
 {
     int a = 3;
-    if(a==3)
+    if(a==4)
     {
         a=4;
+        if(3==4)
+        {
+            a=7;
+        }
+        else
+        {
+            a=70;
+        }
 
     }
     else
     {
-        a=70;
+        a=5;
+        if(a==6){
+            a=8;
+        }else{
+            a=80;
+        }
     }
     return a;
 }
