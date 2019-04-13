6d61bfc153ffb0fb38c277e51584b1d8d02448a3
AnatolyG6 <anatolygasiuk@gmail.com>
2019-04-13 17:49:25 +0200

    merge
    # Conflicts:
    #	AsmWriter.cpp
    #	AsmWriter.h
    #	CMakeLists.txt
    #	Expr.g4
    #	Symbole.h
    #	Visiteur.cpp
    #	Visiteur.h
    #	index.c
    #	main.cpp
    #	test/Test1/resultat.s
    #	test/Test2/resultat.s
    #	test/Test3/resultat.s
    #	test/Test4/resultat.s
    #	test/Test5/resultat.s
    #	test/Test6/resultat.s
    #	test/Test7/resultat.s
    #	test/Test8/resultat.s
    #	test/Test9/resultat.s

--- index.c
+++ index.c
@@ -1,24 +1,14 @@
 int sum()
 {
-    int a;
-    a=1+2*3+4;
-    return a;
+    int b;
+    b=2+3*(1+1);
+    return b;
 }
 
 int main()
 {
-    int a,b,c;
-    a=5;
-    b=7;
-    c=5+(b-a)*2;
+    int a;
     a=sum();
-    if(a==11)
-    {
-        a=11;
-    }
-    else
-    {
-        a=22;
-    }
+
     return a;
 }
--- resultat.s
+++ resultat.s
@@ -5,19 +5,19 @@ sum:
 	pushq	%rbp
 	movq	%rsp, %rbp
 	subq	$16, %rsp
-	movl	$1, -8(%rbp)
-	movl	$2, -12(%rbp)
-	movl	$3, -16(%rbp)
-	movl	-12(%rbp), %edx
-	movl	-16(%rbp), %eax
-	imull	%edx, %eax
-	movl	 %eax, -20(%rbp)
-	movl	-8(%rbp), %edx
+	movl	$2, -8(%rbp)
+	movl	$3, -12(%rbp)
+	movl	$1, -16(%rbp)
+	movl	$1, -20(%rbp)
+	movl	-16(%rbp), %edx
 	movl	-20(%rbp), %eax
 	addl	%edx, %eax
 	movl	 %eax, -24(%rbp)
-	movl	$4, -28(%rbp)
-	movl	-24(%rbp), %edx
+	movl	-12(%rbp), %edx
+	movl	-24(%rbp), %eax
+	imull	%edx, %eax
+	movl	 %eax, -28(%rbp)
+	movl	-8(%rbp), %edx
 	movl	-28(%rbp), %eax
 	addl	%edx, %eax
 	movl	 %eax, -32(%rbp)
@@ -36,12 +36,9 @@ main:
 	pushq	%rbp
 	movq	%rsp, %rbp
 	subq	$16, %rsp
-	movl	$5, -40(%rbp)
-	movl	-40(%rbp), %eax
-	movl	%eax, -4(%rbp)
 	call sum
-	movl %eax, -4(%rbp)
-	movl	-4(%rbp), %eax
+	movl %eax, -40(%rbp)
+	movl	-40(%rbp), %eax
 	movl	%eax, -44(%rbp)
 	movl	-44(%rbp), %eax
 	movq	%rbp, %rsp
